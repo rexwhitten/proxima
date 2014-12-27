1bafa4d90fd1ff9d197942c7d8a91737ab5ba516
rexwhitten <rex.whitten@gmail.com>
2014-12-26 19:51:36 -0500

    basic proxy

--- proxima/server.js
+++ proxima/server.js
@@ -1,23 +1,28 @@
-﻿
-var Options = {
-    identifier: "Proxima 0",
-    topology : [
-        { identifier : "starnode01", url : "http://104.131.77.40/api" , proxy_path: "/sn1" },
-        { identifier : "starnode02", url : "http://104.236.60.234/api" , proxy_path: "/sn2" },
-    ]
-};
+﻿// -------------------------------------------------------------------
+// Proxima - Proxy Server
+// -------------------------------------------------------------------
 
-var httpProxy = require('http-proxy');
+var http = require('http'),
+    url = require('url'),
+    httpProxy = require('http-proxy');
 
-var proxy = httpProxy.createProxy();
+//
+// Create a proxy server with custom application logic
+//
+var proxy = httpProxy.createProxyServer({ target: "/api" });
 
-var options = {
-    '104.236.87.110/api/test': 'http://104.131.77.40/api',
-    '104.236.87.110/api/dev': 'http://104.236.60.234/api'
-}
 
-require('http').createServer(function (req, res) {
-    proxy.web(req, res, {
-        target: options[req.headers.host]
-    });
-}).listen(80);
+//
+// Create your custom server and just call `proxy.web()` to proxy
+// a web request to the target passed in the options
+// also you can use `proxy.ws()` to proxy a websockets request
+//
+var server = http.createServer(function (req, res) {
+    // You can define here your custom logic to handle the request
+    // and then proxy the request.
+    console.log(req.url);
+    proxy.web(req, res, { target: 'http://104.131.77.40/api' });
+});
+
+console.log("listening on port 80")
+server.listen(80);
