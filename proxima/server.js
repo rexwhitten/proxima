a61b16fe224f5cbad910e4dcbe2c4f9ecdfbd689
rexwhitten <rex.whitten@gmail.com>
2014-12-26 19:06:03 -0500

    basic proxy server created

--- proxima/server.js
+++ proxima/server.js
@@ -7,23 +7,20 @@ var Options = {
     ]
 };
 
-
+// Http Objects
 var http = require('http'),
     httpProxy = require('http-proxy');
 
+//
+// Create a proxy server with custom application logic
+//
 var proxy = httpProxy.createProxyServer({});
-
-proxy.on('proxyReq', function (proxyReq, req, res, options) {
-    console.log(req.path);
-    proxyReq.setHeader('X-PROXIMA', "path");
-});
 
 var server = http.createServer(function (req, res) {
     // You can define here your custom logic to handle the request
     // and then proxy the request.
-    var target = "";
-
-    proxy.web(req, res, {
-        target: 'http://104.131.77.40/api/'
-    });
+    proxy.web(req, res, { target: 'http://104.131.77.40/api' });
 });
+
+console.log("listening on port 5050")
+server.listen(80);
